4cb6b7baa00a7d9961f5d6d47dfedbe6f790ac56
1967-ROB <124263833+1967-ROB@users.noreply.github.com>
2025-07-26 09:02:38 -0300

    Update script-lomitos.js

--- script-lomitos.js
+++ script-lomitos.js
@@ -1,16 +1,24 @@
 fetch('/data/precios.json')
   .then(response => response.json())
   .then(data => {
-const menu = document.getElementById('menu-container');
+    const menu = document.getElementById('menu-container');
     Object.entries(data).forEach(([nombre, item]) => {
       const div = document.createElement('div');
-      div.className = 'producto';
+      div.className = 'product';
       div.innerHTML = `
-        <img src="/img-pizza/${item.imagen}" alt="${nombre}">
+        <img src="/img-lomitos/${item.imagen}" alt="${nombre}">
         <h3>${nombre}</h3>
         <p>${item.ingredientes}</p>
         <p><strong>${item.precio.toLocaleString()} Gs</strong></p>
       `;
+      div.addEventListener('click', () => {
+        document.getElementById('popup-title').textContent = nombre;
+        document.getElementById('cantidad').value = 1;
+        document.getElementById('popup').style.display = 'flex';
+        document.getElementById('overlay').style.display = 'block';
+        div.dataset.nombre = nombre;
+        div.dataset.precio = item.precio;
+      });
       menu.appendChild(div);
     });
   })
